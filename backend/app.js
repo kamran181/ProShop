607ef4abc68ddc018d8c6b96c0b0bef3d2715072
Kamran Khan <kamrankhan@Kamrans-MacBook-Air.local>
2024-05-23 12:26:02 +0530

    proshop project

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,6 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+app.listen(8080, () => {
     console.log(`server is running in ${process.env.NODE_ENV} on port ${PORT}!`);
 })
